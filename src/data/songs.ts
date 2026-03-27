2caf7ff782efef862eeddb6876c411c76bf1edd7
GorizontIDC <151389770+GorizontIDC@users.noreply.github.com>
2026-03-27 12:52:35 +0700

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -54,6 +54,42 @@ export const listeningNow: Song[] = [
     duration: '6:30',
     album: 'Hotel California',
     year: 1976
+  },
+   {
+    id: 1,
+    title: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    cover: bohCober,
+    duration: '5:55',
+    album: 'A Night at the Opera',
+    year: 1975,
+  },
+  {
+    id: 2,
+    title: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    cover: '/JAMusic/images/queen.jpg',
+    duration: '5:55',
+    album: 'A Night at the Opera',
+    year: 1975,
+  },
+  {
+    id: 3,
+    title: 'Venice Escape',
+    artist: 'Yesper Cude',
+    cover: Ven,
+    duration: '3:29',
+    album: 'Ambient Dreams',
+    year: 2023,
+  },
+  {
+    id: 4,
+    title: 'Hotel California',
+    artist: 'Eagles',
+    cover: hotel,
+    duration: '6:30',
+    album: 'Hotel California',
+    year: 1976
   }
 ]
 export const newReleases: Song[] = [
