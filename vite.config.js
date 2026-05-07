41fd6e399e925b58155d6d712b2f6212f2973bfa
GorizontIDC <151389770+GorizontIDC@users.noreply.github.com>
2026-05-07 21:17:04 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://lzuujoprghpejxfjtvxx.supabase.co',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
-})
+    base: './',
+    build:{
+    outDir: 'dist'}
+    })
